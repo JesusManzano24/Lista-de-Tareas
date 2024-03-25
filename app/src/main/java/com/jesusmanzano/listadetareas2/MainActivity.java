52e3c06ea392353a01fbd570b148f1e7446a356f
JesusManzano24 <9991404825j@gmail.com>
2024-03-25 17:49:13 -0600

    Segundo Commit Funcion Loggin

--- app/src/main/java/com/jesusmanzano/listadetareas2/MainActivity.java
+++ app/src/main/java/com/jesusmanzano/listadetareas2/MainActivity.java
@@ -2,13 +2,39 @@ package com.jesusmanzano.listadetareas2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editTextUser;
+    EditText editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextUser = findViewById(R.id.user);
+        editTextPassword = findViewById(R.id.password);
+
+        Button Inicio = findViewById(R.id.Inicio);
+        Inicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = editTextUser.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                if (user.equals("Jesus") && password.equals("1234")) {
+                    Intent intent = new Intent(v.getContext(), MainActivity2.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
